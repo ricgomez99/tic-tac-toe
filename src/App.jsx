46c7eeaee6fccdb038dfcddac3c3fcb4a5de34a0
Andres <ricardo.gomez.restrepo@gmail.com>
2023-11-18 19:02:32 -0500

    winner logic added

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,54 @@ const TURNS = {
   o: "o",
 };
 
+const WINNER_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.x);
+  const [winner, setWinner] = useState(null); // null: No winner, false: Draw
 
   const updateBoard = (index) => {
     // If the position already has a value
-    // We won't overwrite it
-    if (board[index]) return;
+    // We won't overwrite it OR if there's a winner
+    if (board[index] || winner) return;
     // Update the Board
     const newBoard = [...board];
-    newBoard[index] = turn; // x u o
+    newBoard[index] = turn; // x / o
     setBoard(newBoard);
 
     const newTurn = turn === TURNS.x ? TURNS.o : TURNS.x;
     setTurn(newTurn);
+
+    // Checks for a winner
+    const newWinner = winnerCheck(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    }
+  };
+
+  const winnerCheck = (boardToCheck) => {
+    for (const combo of WINNER_COMBINATIONS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] && // Checks if position [0] is true (x u o)
+        boardToCheck[a] === boardToCheck[b] && // Checks if at [0] & [3] there's x -> x / o -> 0
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]; // Should return x / o
+      }
+    }
+    // No winner
+    return null;
   };
 
   return (
